3c3b863638d53a13e3a550e1b3586b9b1ae89bbd
Felipe <felipepg@uniplaclages.edu.br>
2020-09-29 21:32:53 -0300

    Criado formulário Cliente

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 
 from mod_home.home import bp_home
+from mod_cliente.cliente import bp_cliente
 
 app = Flask(__name__)
 app.register_blueprint(bp_home)
+app.register_blueprint(bp_cliente)
 
 
 
