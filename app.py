22f211e78aa8dafee41b77750eeb7b0c37f0dfd4
Felipe <felipepg@uniplaclages.edu.br>
2020-09-30 20:56:29 -0300

    Criado formulário funcionários

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 
 from mod_home.home import bp_home
 from mod_cliente.cliente import bp_cliente
+from mod_funcionario.funcionario import bp_funcionario
 
 app = Flask(__name__)
 app.register_blueprint(bp_home)
 app.register_blueprint(bp_cliente)
+app.register_blueprint(bp_funcionario)
 
 
 
