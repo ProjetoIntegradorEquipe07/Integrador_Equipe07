dd3f7fca9ad0f68f37ebca1fb7d789dea7e38572
Felipe <felipepg@uniplaclages.edu.br>
2020-09-30 21:11:49 -0300

    Formulário produto close #4

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask
 from mod_home.home import bp_home
 from mod_cliente.cliente import bp_cliente
 from mod_funcionario.funcionario import bp_funcionario
+from mod_produto.produto import bp_produto
 
 app = Flask(__name__)
 app.register_blueprint(bp_home)
 app.register_blueprint(bp_cliente)
 app.register_blueprint(bp_funcionario)
+app.register_blueprint(bp_produto)
 
 
 
