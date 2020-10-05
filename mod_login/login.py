a34de0c7f99061973434f51c99149c1ef48c04f3
Felipe <felipepg@uniplaclages.edu.br>
2020-10-05 11:18:54 -0300

    close #25

--- mod_login/login.py
+++ mod_login/login.py
@@ -1,10 +1,30 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, session
+from functools import wraps
 
 bp_login = Blueprint('login', __name__, url_prefix="/", template_folder='templates')
+
+# valida se o usuário esta ativo na sessão
+def validaSessao(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'usuario' not in session:
+            #descarta os dados copiados da função original e retorna a tela de login
+            return redirect(url_for('login.login', falhaSessao=1))
+        else:
+            #retorna os dados copiados da função original
+            return f(*args, **kwargs)
+
+    #retorna o resultado do if acima
+    return decorated_function
 
 @bp_login.route("/")
 def login():
     return render_template('formLogin.html')
+
+@bp_login.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for('login.login'))
 
 @bp_login.route("/login", methods=['POST'])
 def validaLogin():
@@ -12,6 +32,8 @@ def validaLogin():
     _senha = request.form['senha']
 
     if _nome == 'admin' and _senha == 'admin':
+        session.clear()
+        session['usuario'] = _nome
         return redirect(url_for('home.home'))
 
     else:
--- mod_produto/produto.py
+++ mod_produto/produto.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, render_template
 
+from mod_login.login import validaSessao
+
 bp_produto = Blueprint('produto', __name__, url_prefix='/produto', template_folder='templates')
 
 @bp_produto.route("/")
+@validaSessao
 def formListaProdutos():
     return render_template('formListaProdutos.html')
 
 @bp_produto.route("formProduto")
+@validaSessao
 def formProduto():
     return render_template('formProduto.html')
