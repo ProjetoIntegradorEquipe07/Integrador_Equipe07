4632dcae9f84de4a6d3646627ed03ea2eef8a731
Felipe <felipepg@uniplaclages.edu.br>
2020-10-08 15:22:19 -0300

    Conexão feita

--- mod_cliente/cliente.py
+++ mod_cliente/cliente.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, render_template
 
 from mod_login.login import validaSessao
+from mod_cliente.clienteBD import Cliente
 
 bp_cliente = Blueprint('cliente', __name__, url_prefix='/cliente', template_folder='templates')
 
 @bp_cliente.route("/")
 @validaSessao
 def formListaClientes():
+    cliente = Cliente()
+    cliente.selectAll()
     return render_template('formListaClientes.html')
 
 @bp_cliente.route("/formCliente")
---
+++ mod_cliente/clienteBD.py
@@ -0,0 +1,26 @@
+from bancoBD import Banco
+
+class Cliente():
+    def __init__(self, id_cliente=0, nome="", cpf="", telefone="", compra_fiado=0, senha="", dia_fiado=1):
+        self.id_cliente = id_cliente
+        self.nome = nome
+        self.cpf = cpf
+        self.telefone = telefone
+        self.compra_fiado = compra_fiado
+        self.senha = senha
+        self.dia_fiado = dia_fiado
+
+    def selectAll(self):
+        try:
+            banco = Banco()
+
+            c = banco.conexao.cursor()
+            c.execute('SELECT id_cliente, nome, telefone FROM tb_clientes')
+
+            result = c.fetchall()
+            c.close()
+
+            return result
+
+        except:
+            return 'Erro ao conectar no banco!'
