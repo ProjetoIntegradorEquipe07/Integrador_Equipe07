3df8be27d217e35a51b03ba976fd5b680a9194db
Felipe <felipepg@uniplaclages.edu.br>
2020-10-10 16:24:28 -0300

    SELECT funcionario feito

--- mod_funcionario/funcionario.py
+++ mod_funcionario/funcionario.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, render_template
 
 from mod_login.login import validaSessao
+from mod_funcionario.funcionarioBD import Funcionario
 
 bp_funcionario = Blueprint('funcionario', __name__, url_prefix="/funcionario", template_folder="templates")
 
 @bp_funcionario.route("/")
 @validaSessao
 def formListaFuncionarios():
-    return render_template('formListaFuncionarios.html')
+    funcionario = Funcionario()
+    _funcionarios = funcionario.selectAll()
+    return render_template('formListaFuncionarios.html', funcionarios = _funcionarios)
 
 @bp_funcionario.route("/formFuncionario")
 @validaSessao
---
+++ mod_funcionario/funcionarioBD.py
@@ -0,0 +1,97 @@
+from bancoBD import Banco
+
+class Funcionario():
+    def __init__(self, id_funcionario=0, nome="", cpf="", telefone="", senha="", matricula="", grupo=2):
+        self.id_funcionario = id_funcionario
+        self.nome= nome
+        self.cpf = cpf
+        self.telefone = telefone
+        self.senha = senha
+        self.matricula = matricula
+        self.grupo = grupo
+
+    def selectAll(self):
+        try:
+            banco = Banco()
+
+            c = banco.conexao.cursor()
+
+            c.execute('SELECT id_funcionario, nome, matricula, grupo FROM tb_funcionario')
+
+            result = c.fetchall()
+            c.close()
+            return result
+        except Exception as e:
+            return 'Erro ao buscar funcionários!'
+
+    def selectOne(self):
+        try:
+            banco = Banco()
+
+            c = banco.conexao.cursor()
+            c.execute('SELECT id_funcionario, nome, cpf, telefone, senha, matricula, grupo FROM tb_funcionario where id_funcionario = %s',(self.id_funcionario))
+
+            for linha in c:
+                self.id_funcionario = linha[0]
+                self.nome = linha[1]
+                self.cpf = linha[2]
+                self.telefone = linha[3]
+                self.senha = linha[4]
+                self.matricula = linha[5]
+                self.grupo = linha[6]
+
+            c.close()
+
+            return 'Busca feita com sucesso!'
+
+        except:
+            return 'Erro ao buscar cliente!'
+
+    def insert(self):
+        try:
+            banco = Banco()
+
+            c = banco.conexao.cursor()
+
+            c.execute('INSERT INTO tb_funcionario(nome,cpf,telefone,senha,matricula,grupo) VALUES(%s,%s,%s,%s,%s,%s)',(self.nome,self.cpf,self.telefone,self.senha,self.matricula,self.grupo))
+            banco.conexao.commit()
+            c.close()
+
+            return 'Funcionário cadastrado com sucesso!'
+
+        except Exception as e:
+            raise Exception('Erro ao cadastrar funcionário', str(e))
+    
+    def update(self):
+        try:
+            banco = Banco()
+
+            c = banco.conexao.cursor()
+
+            c.execute('UPDATE tb_funcionario SET nome=%s,cpf=%s,telefone=%s,senha=%s,matricula=%s,grupo=%s WHERE id_funcionario=%s',(self.nome,self.cpf,self.telefone,self.senha,self.matricula,self.grupo,self.id_funcionario))
+            banco.conexao.commit()
+            c.close()
+
+            return 'Funcionário editado com sucesso!'
+
+        except Exception as e:
+            raise Exception('Erro ao editar funcionário!', str(e))
+
+    def delete(self):
+        try:
+            banco = Banco()
+
+            c = banco.conexao.cursor()
+
+            c.execute('DELETE FROM tb_funcionario WHERE id_funcionario = %s',(self.id_funcionario))
+
+            banco.conexao.commit()
+            c.close()
+
+            return 'Funcionário excluído com sucesso'
+
+        except Exception as e:
+            raise Exception('Erro ao excluir funcionário', str(e))
+
+
+
