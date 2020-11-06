5f3e55e0175cf47c896cafa9c168121c532cf3ba
Marcos Lima <marcos.lima@alwitecnologia.com>
2020-11-06 00:41:59 -0300

    Alterações menu

---
+++ static/js/estilo.js
@@ -0,0 +1,20 @@
+    $(".itens-principal").toggle();
+    $(document).ready(function(){
+        $("#principal").click(function(){
+        $(".itens-principal").toggle();
+        });
+    });
+
+    $(".itens-administrador").toggle();
+    $(document).ready(function(){
+        $("#administrador").click(function(){
+        $(".itens-administrador").toggle();
+        });
+    });
+
+    $(".itens-comandas").toggle();
+    $(document).ready(function(){
+        $("#comandas").click(function(){
+        $(".itens-comandas").toggle();
+        });
+    });
