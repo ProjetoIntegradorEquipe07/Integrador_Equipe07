c31253976eda35e91c92690d763330bb20772639
Felipe <felipepg@uniplaclages.edu.br>
2020-11-10 10:31:08 -0300

    Funcao log feita

--- mod_login/login.py
+++ mod_login/login.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, sessio
 from functools import wraps
 
 from mod_funcionario.funcionarioBD import Funcionario
-from funcoes import Funcoes
+from funcoes import Funcoes, LOG
 
 bp_login = Blueprint('login', __name__, url_prefix="/", template_folder='templates')
 
@@ -59,11 +59,23 @@ def validaLogin():
             session['usuario'] = funcionario.nome
             session['grupo'] = funcionario.grupo
             session['id'] = funcionario.id_funcionario
+
+            Funcoes.criaLOG('Login feito com sucesso!', LOG.info)
+
             return jsonify(erro = False, nome = funcionario.nome)
 
         else:
+            session['id'] = 0
+            Funcoes.criaLOG('Usuario ou senha incorretos', LOG.warning)
+            session.clear()
             return jsonify(erro = True)
     except Exception as e:
-        _mensagem, _mensagem_exception = e.args
-
+        session.clear()
+        if len(e.args) > 1:
+            _mensagem, _mensagem_exception = e.args
+            Funcoes.criaLOG(_mensagem_exception, LOG.error)
+        else:
+            _mensagem = 'Erro'
+            _mensagem_exception = str(e)
+            Funcoes.criaLOG(_mensagem_exception, 'error')
         return jsonify(erro_ex = True, mensagem = _mensagem, mensagem_exception = _mensagem_exception)
