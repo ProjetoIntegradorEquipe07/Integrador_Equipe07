19bfd14de2f21a30334aa452c7a80ef3a919682a
Felipe <felipepg@uniplaclages.edu.br>
2020-11-21 09:47:29 -0300

    form erro 404 feito

---
+++ mod_erro/erro.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+from mod_login.login import validaSessao
+
+bp_erro = Blueprint('erro', __name__, url_prefix='/', template_folder='templates')
+
+@bp_erro.app_errorhandler(404)
+@validaSessao
+def erro404(error):
+    return render_template('form_erro_404.html'), 404
+
+@bp_erro.app_errorhandler(500)
+@validaSessao
+def erro500(error):
+    return render_template('form_erro_500.html'), 500
