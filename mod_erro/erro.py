94e7bf64f3df9ffc0b1ad97c1164aee6acbe7fd8
Felipe <felipepg@uniplaclages.edu.br>
2020-11-21 09:57:42 -0300

    close #79

--- mod_erro/erro.py
+++ mod_erro/erro.py
@@ -7,9 +7,9 @@ bp_erro = Blueprint('erro', __name__, url_prefix='/', template_folder='templates
 @bp_erro.app_errorhandler(404)
 @validaSessao
 def erro404(error):
-    return render_template('form_erro_404.html'), 404
+    return render_template('form_erro_404.html')
 
 @bp_erro.app_errorhandler(500)
 @validaSessao
 def erro500(error):
-    return render_template('form_erro_500.html'), 500
+    return render_template('form_erro_500.html')
